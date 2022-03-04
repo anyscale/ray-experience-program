5a13f6d44502e209d1ed1018b7e3d749e42eca9d
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-03-04 12:43:50 -0800

    add comments

--- train_xgboost/starter.py
+++ train_xgboost/starter.py
@@ -12,17 +12,31 @@ xgboost_params = {
 
 
 def train_xgboost(
-    config, data: pd.DataFrame, target_column: str, test_fraction: float = 0.3
+    config: dict, data: pd.DataFrame, target_column: str, test_fraction: float = 0.3
 ):
+    """
+    Train an XGBoost model on the data specified in the `data` arg.
+
+    Args:
+        config (Dict): A dictionary for XGBoost specific configurations.
+        data: (pd.DataFrame): A pandas dataframe containing the data to train on.
+        target_column (str): The name of the column in the dataframe to use as the labels.
+        test_fraction (float): What fraction of the data to use as the test set. 
+            The test set data will be used only for evaluation and not for training. 
+    """
+
+    # Get the current time. We will use this later to see how long training takes.
     start_time = time.time()
 
-    # Split data into train and test
+    # Split data into train and test.
+    # First sample 1-test_fraction of the data to use as the training set.
     X_train = data.sample(frac=1 - test_fraction)
-    X_valid = data.drop(X_train.index)
+    # Then take the remainder of the data and use that as the test set.
+    X_test = data.drop(X_train.index)
 
-    # Pass Pandas dataframe to DMatrix
+    # Pass Pandas dataframe to DMatrix. 
     train_set = DMatrix(X_train.drop(target_column, axis=1), X_train[target_column])
-    test_set = DMatrix(X_valid.drop(target_column, axis=1), X_valid[target_column])
+    test_set = DMatrix(X_test.drop(target_column, axis=1), X_test[target_column])
 
     evals_result = {}
 
@@ -44,7 +58,7 @@ def train_xgboost(
 
     return bst
 
-
+# Use pandas to read the parquet files in the ./data directory to a pandas dataframe.
 data = pd.read_parquet(
     "./data"
 )
